eb27ce5156ecafcd4e2bed58180bdddbfa7eba51
adrivn <adrian.emege@gmail.com>
2024-05-03 16:14:30 +0200

    added satellite.nvim and dropbar.nvim

--- lua/plugins/_ui.lua
+++ lua/plugins/_ui.lua
@@ -1,14 +1,8 @@
 return {
   -- highlight undo
-  {
-    "tzachar/highlight-undo.nvim",
-    opts = {},
-  },
+  { "tzachar/highlight-undo.nvim", opts = {} },
   -- better escape from insert mode using j & k
-  {
-    "max397574/better-escape.nvim",
-    opts = {},
-  },
+  { "max397574/better-escape.nvim", opts = {} },
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
@@ -16,6 +10,15 @@ return {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
-    }
+    },
+  },
+  { "lewis6991/satellite.nvim", event = "VeryLazy", opts = {} },
+  {
+    "Bekaboo/dropbar.nvim",
+    -- optional, but required for fuzzy finder support
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+    },
   },
 }
